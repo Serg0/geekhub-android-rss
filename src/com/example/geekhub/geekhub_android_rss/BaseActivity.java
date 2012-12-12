e46911f771554633ed94771ab5885ca42d08f5a8
clans <dmitry.tarianyk@masterofcode.com>
2012-12-12 14:51:38 +0200

    Added Libraries

--- src/com/example/geekhub/geekhub_android_rss/BaseActivity.java
+++ src/com/example/geekhub/geekhub_android_rss/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.example.geekhub.geekhub_android_rss;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import org.holoeverywhere.app.Activity;
+import org.holoeverywhere.app.AlertDialog;
+import org.holoeverywhere.widget.Button;
+import org.holoeverywhere.widget.EditText;
 
 public class BaseActivity extends Activity {
     /**
@@ -11,5 +15,21 @@ public class BaseActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final EditText edit = (EditText) findViewById(R.id.editText);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(BaseActivity.this);
+                dialog.setTitle("Dialog title");
+                dialog.setMessage(edit.getText());
+                dialog.setPositiveButton("OK", null);
+                dialog.show();
+            }
+        });
+
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
